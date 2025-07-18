a10d68e4e39a9ea70a2e9c3dcb02d18a2ee087f3
Fred <algopt.fr@gmail.com>
2025-07-18 10:52:21 +0200

    first

---
+++ src/polyfills.ts
@@ -0,0 +1,21 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * This setup is for browsers supporting ES2015+.
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// (Empty for Angular 17+, add here if needed)
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
